85ca042282239f6a629adfc3d0fd7ac31f7f0d87
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-07-14 19:34:33 +0200

    fuck typescript

--- index.ts
+++ index.ts
@@ -1,24 +1,35 @@
-import { Client, Intents }  from 'discord.js';
-const {token} = require('./config.json')
-
+import { Client, Collection, Intents } from "discord.js";
+import { spawn } from "child_process";
+const { token } = require("./config.json");
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.once('ready', () => {
-	console.log('Ready!');
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
+  const { commandName } = interaction;
+  switch (commandName) {
+    case "ping":
+        interaction.reply("pong");
+    case 'exec':
+        interaction.reply("pong");
+  }
 });
 
 client.login(token);
-
--- register-commands.ts
+++ register-commands.ts
@@ -1,17 +1,23 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+const { clientId, guildId, token } = require("./config.json");
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
