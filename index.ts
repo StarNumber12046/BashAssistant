c18dd2d2bbdabb574079fe12a4923ddebc0fd5df
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-07-14 19:19:53 +0200

    working with typescript

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { Client, Intents }  from 'discord.js';
+const {token} = require('./config.json')
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
+
